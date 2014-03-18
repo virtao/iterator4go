644e3cf29f4dc9323628844465e2c51a0a2a7f89
Virtao <virtao@virtao.org>
2014-03-18 15:59:21 +0800

    Add text file iterator. Update README.md.

--- README.md
+++ README.md
@@ -8,6 +8,7 @@ A sample code:
 package main
 
 import (
+    "fmt"
     it "github.com/virtao/iterator4go"
     "math/rand"
 )
@@ -15,6 +16,7 @@ import (
 func main() {
     useIterator()
     useIntIterator()
+    useTxtFileIterator("D:\\test.txt")
 }
 
 const dataSize int = 1000000
@@ -56,7 +58,25 @@ func useIntIterator() {
         sum += itor.Value()
     }
 }
+
+func useTxtFileIterator(filePath string) {
+    if itor := it.NewTxtFileIterator(filePath); itor == nil {
+        fmt.Println("文件打开失败。")
+    } else {
+        defer itor.Close() //使用文本迭代器一定别忘了使用Close()函数。
+        defer func() {
+            if r := recover(); r != nil {
+                fmt.Println("Panic : ", r)
+            }
+        }()
+        for itor.Next() {
+            fmt.Println(itor.Value())
+        }
+    }
+}
 ```
 
 Note:
 interface{} is slow. If the type is known, then please try to use explicit type.
+
+After calling the TxtFileIterator is complete, you must not forget to call Close() function.
--- iterator.go
+++ iterator.go
@@ -1,5 +1,13 @@
 package iterator4go
 
+import (
+	"bufio"
+	"bytes"
+	"errors"
+	"io"
+	"os"
+)
+
 type Iterator struct {
 	current int
 	data    []interface{}
@@ -38,4 +46,69 @@ func (it *IntIterator) Next() bool {
 	}
 	it.current++
 	return true
+}
+
+type TxtFileIterator struct {
+	current int
+	data    string
+	reader  *bufio.Reader
+	file    *os.File
+}
+
+var ErrReadFileFailed = errors.New("TxtFileIterator : ErrReadFileFailed")
+
+func NewTxtFileIterator(filePath string) *TxtFileIterator {
+	if fr, err := os.Open(filePath); err != nil {
+		return nil
+	} else {
+		return &TxtFileIterator{data: "", reader: bufio.NewReader(fr), file: fr, current: 0}
+	}
+}
+
+func (it *TxtFileIterator) Value() string {
+	return it.data
+}
+
+//注意，此方法可能产生panic，分别是bytes.ErrTooLarge、iterator4go.ErrReadFileFailed
+func (it *TxtFileIterator) Next() bool {
+	var data []byte
+	var isPrefix bool
+	var err error
+
+	buf := new(bytes.Buffer)
+
+	for {
+
+		if data, isPrefix, err = it.reader.ReadLine(); err == nil {
+			buf.Write(data)
+			if isPrefix == true {
+				continue
+			}
+			it.data = string(buf.Bytes())
+			it.current++
+			return true
+		} else {
+			if err == io.EOF {
+				it.data = ""
+				return false
+			} else {
+				panic(ErrReadFileFailed)
+			}
+		}
+
+	}
+
+	if it.current+1 >= len(it.data) {
+		return false
+	}
+	it.current++
+	return true
+}
+
+func (it *TxtFileIterator) Close() {
+	it.reader = nil
+	if it.file != nil {
+		it.file.Close()
+		it.file = nil
+	}
 }
